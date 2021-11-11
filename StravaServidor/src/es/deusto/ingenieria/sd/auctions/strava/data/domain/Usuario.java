b97bd22a6d7611f2b8db890b76695e4c0c5ce5fe
tzabar <tzabar@gmail.com>
2021-11-11 13:34:00 +0100

    cambios usuario

--- StravaServidor/src/es/deusto/ingenieria/sd/auctions/strava/data/domain/Usuario.java
+++ StravaServidor/src/es/deusto/ingenieria/sd/auctions/strava/data/domain/Usuario.java
@@ -1,29 +1,39 @@
 package es.deusto.ingenieria.sd.auctions.strava.data.domain;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
+import es.deusto.ingenieria.sd.auctions.server.data.domain.Article;
+import es.deusto.ingenieria.sd.auctions.server.data.domain.Bid;
+
 public class Usuario {	
-	private String nickname;
-	private String password;
+	private int idUsuario;
+	private String nombre;
 	private String email;
-	private List<Bid_CambiarAEstado> bids = new ArrayList<>();
-	private List<Article_CambiarAentrenamiento> articles = new ArrayList<>();
-		
-	public String getNickname() {
-		return nickname;
+	private Date fechaNacimiento;
+	private String contrasenya;
+	/*
+	 * faltan las clases relacionadas
+	 * 	private List<Bid> bids = new ArrayList<>();
+	private List<Article> articles = new ArrayList<>();
+
+	 */
+	
+	public String getnombre() {
+		return nombre;
 	}
 	
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 	
-	public boolean checkPassword(String password) {
-		return this.password.equals(password);
+	public boolean checkcontrasenya(String contrasenya) {
+		return this.contrasenya.equals(contrasenya);
 	}
 	
-	public void setPassword(String password) {
-		this.password = password;
+	public void setContrasenya(String contrasenya) {
+		this.contrasenya = contrasenya;
 	}
 	
 	public String getEmail() {
@@ -34,46 +44,62 @@ public class Usuario {
 		this.email = email;
 	}
 	
-	public List<Bid_CambiarAEstado> getBids() {
-		return bids;
-	}
+//	public List<Bid_CambiarAEstado> getBids() {
+//		return bids;
+//	}
+//	
+//	public void setBids(List<Bid_CambiarAEstado> bids) {
+//		this.bids = bids;
+//	}
 	
-	public void setBids(List<Bid_CambiarAEstado> bids) {
-		this.bids = bids;
-	}
-	
-	public void addBid(Bid_CambiarAEstado bid) {
-		if (bid != null && !this.bids.contains(bid)) {
-			this.bids.add(bid);
-		}
-	}
-	
-	public List<Article_CambiarAentrenamiento> getArticles() {
-		return articles;
-	}
-	
-	public void setArticles(List<Article_CambiarAentrenamiento> articles) {
-		this.articles = articles;
-	}
-	
-	public void addArticle(Article_CambiarAentrenamiento article) {
-		if (article != null && !this.articles.contains(article)) {
-			this.articles.add(article);
-		}
-	}
+//	public void addBid(Bid_CambiarAEstado bid) {
+//		if (bid != null && !this.bids.contains(bid)) {
+//			this.bids.add(bid);
+//		}
+//	}
+//	
+//	public List<Article_CambiarAentrenamiento> getArticles() {
+//		return articles;
+//	}
+//	
+//	public void setArticles(List<Article_CambiarAentrenamiento> articles) {
+//		this.articles = articles;
+//	}
+//	
+//	public void addArticle(Article_CambiarAentrenamiento article) {
+//		if (article != null && !this.articles.contains(article)) {
+//			this.articles.add(article);
+//		}
+//	}
 		
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
 	@Override
 	public String toString() {
 		StringBuffer result = new StringBuffer();
 		
-		result.append(this.nickname);
+		result.append(this.nombre);
 		result.append(" - ");
 		result.append(this.email);
 		result.append(" - (");
-		result.append(this.articles.size());
-		result.append(" articles) - (");
-		result.append(this.bids.size());
-		result.append(" bids)");
+//		result.append(this.articles.size());
+//		result.append(" articles) - (");
+//		result.append(this.bids.size());
+//		result.append(" bids)");
 		
 		return result.toString();
 	}
