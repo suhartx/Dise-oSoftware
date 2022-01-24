f039c4e32c3d16aa1d32825e906533f12a79c688
suhartx <suhar.txabarri@bosonit.com>
2022-01-24 17:12:30 +0100

    commit

--- StravaServidor/src/es/deusto/ingenieria/sd/strava/data/domain/Reto.java
+++ StravaServidor/src/es/deusto/ingenieria/sd/strava/data/domain/Reto.java
@@ -2,7 +2,6 @@ package es.deusto.ingenieria.sd.strava.data.domain;
 
 import java.util.Date;
 
-import javax.jdo.annotations.Join;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
